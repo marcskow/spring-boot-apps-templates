53401a8c07711f8be1d6ea3cb29377793a497713
Marcin Skowron <mskowron17@gmail.com>
2019-02-26 21:28:11 +0100

    Added ElasticSearch template (will be probably developed as another repository)

---
+++ spring-bootstrap-elasticsearch/src/main/java/com/marcskow/spring/bootstrap/java/BootstrapJavaApplication.java
@@ -0,0 +1,13 @@
+package com.marcskow.spring.bootstrap.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapJavaApplication.class, args);
+	}
+
+}
