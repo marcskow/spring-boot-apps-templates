b8aca36db996dfbfab73beda75976f3bba3d7382
Marcin Skowron <mskowron17@gmail.com>
2019-04-24 21:45:54 +0200

    Change module name and properties

--- spring-bootstrap-java-audit-swagger/src/main/java/com/marcskow/spring/bootstrap/java/SpringBootAuditSwagger.java
+++ spring-bootstrap-java-audit-swagger/src/main/java/com/marcskow/spring/bootstrap/java/SpringBootAuditSwagger.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class BootstrapJavaApplication {
+public class SpringBootAuditSwagger {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootstrapJavaApplication.class, args);
+		SpringApplication.run(SpringBootAuditSwagger.class, args);
 	}
 
 }
